ff249b87dcaff8a60a031300b9620d6bb9a62b71
Ajay-sen <ajaysen4736@gmail.com>
2024-09-10 09:02:18 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, // Adjust if necessary
+    rollupOptions: {
+      // external: ['three-stdlib'] // Exclude problematic library
+    }
+  }
 })
